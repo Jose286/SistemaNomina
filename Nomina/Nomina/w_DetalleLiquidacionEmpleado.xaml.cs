5a0677b063d534c2caab3b3c130844c59605d6f3
juliortizpy <49462769+juliortizpy@users.noreply.github.com>
2019-07-10 16:52:32 -0400

    Detalle de liquidacion por empleado 2

--- Nomina/Nomina/w_DetalleLiquidacionEmpleado.xaml.cs
+++ Nomina/Nomina/w_DetalleLiquidacionEmpleado.xaml.cs
@@ -19,9 +19,61 @@ namespace Nomina
     /// </summary>
     public partial class w_DetalleLiquidacionEmpleado : Window
     {
+    
+    NominaEntities datos;
         public w_DetalleLiquidacionEmpleado()
         {
             InitializeComponent();
+            datos = new NominaEntities();
+        }
+        
+         private void btnVerDetalle_Click(object sender, RoutedEventArgs e)
+        {
+            if ((cboEmpleado.SelectedItem != null) &&  (cboLiquidacion.SelectedItem != null))
+            {
+                Empleado ee = (Empleado)cboEmpleado.SelectedItem;
+                Liquidacion_Mensual ll = (Liquidacion_Mensual)cboLiquidacion.SelectedItem;
+
+                var liquidacionDetalle = datos.Liquidacion_Mensual_Detalle.ToList().FindAll(x => x.Liquidacion_Id == ll.Id_Liquidacion && x.Empleado_Id == ee.Id_Empleado);
+                dgDetalle.ItemsSource = liquidacionDetalle;
+
+                var resumenLiquidacion = datos.ResumenLiquidacion.ToList().FindAll(x => x.Id_Liquidacion == ll.Id_Liquidacion && x.Id_Empleado == ee.Id_Empleado);
+                if (resumenLiquidacion.Count == 1)
+                {
+                    foreach (ResumenLiquidacion rr in resumenLiquidacion)
+                    {
+                        lblEgresos.Content = rr.MontoEgreso.ToString();
+                        lblIngresos.Content = rr.MontoIngreso.ToString();
+                        lblTotal.Content = (rr.MontoIngreso - rr.MontoEgreso).ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error, favor comuniquese con informatica.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar el Empleado y la Liquidacion para visualizar el detalle.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void cargarCombos()
+        {
+            var lstUsuarios = datos.Empleado.ToList();
+            cboEmpleado.ItemsSource = lstUsuarios;
+            cboEmpleado.DisplayMemberPath = "Nombres";
+            cboEmpleado.SelectedValuePath = "Id_Empleado";
+
+            var lstConceptos = datos.Liquidacion_Mensual.ToList();
+            cboLiquidacion.ItemsSource = lstConceptos;
+            cboLiquidacion.DisplayMemberPath = "Descripcion";
+            cboLiquidacion.SelectedValuePath = "Id_Concepto";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cargarCombos();
         }
     }
 }
